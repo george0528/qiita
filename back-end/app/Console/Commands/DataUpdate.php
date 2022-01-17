0e8d8cb2ed972088e084e45a34ae04b74c22485c
jota <miki1jota@gmail.com>
2022-01-17 13:50:50 +0900

    fix: b データ更新時のメール送信のバグを修正

--- back-end/app/Console/Commands/DataUpdate.php
+++ back-end/app/Console/Commands/DataUpdate.php
@@ -111,7 +111,8 @@ class DataUpdate extends Command
       }
     }
     Log::alert("データを更新しました");
-    Mail::send('email.send', '', function($message) {
+    $now = Carbon::now();
+    Mail::send('email.send', ['time' => $now], function($message) {
         $message->to('aleph0528@gmail.com')
                 ->from('aleph0528@gmail.com', 'qiita-my-ranking')
                 ->subject('Qiitaの週間ランキングのデータを更新しました');
