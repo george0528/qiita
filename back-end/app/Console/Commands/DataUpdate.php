e6059f041f26fe1395f89e3ec35b09c73d7f72b0
jota <miki1jota@gmail.com>
2022-02-12 02:41:23 +0900

    refactor: データ更新のリファクタリング

--- back-end/app/Console/Commands/DataUpdate.php
+++ back-end/app/Console/Commands/DataUpdate.php
@@ -2,7 +2,6 @@
 
 namespace App\Console\Commands;
 
-use App\Models\Post;
 use Carbon\Carbon;
 use GuzzleHttp\Client;
 use Illuminate\Console\Command;
@@ -13,14 +12,10 @@ use Illuminate\Support\Facades\Mail;
 class DataUpdate extends Command
 {
   // クラス変数
-  public $url = 'https://qiita.com/api/v2/items/';
-  public $token = '5fbe77e0acd1cb34b2d2e0877b68fab6729ca251';
-  public $page = 1;
-  public $per_page = 20;
-  public $data = [];
-  public $query_params = '';
-  public $loop_flag = true;
-  public $contents = [];
+  private $url = 'https://qiita.com/api/v2/items/';
+  private $page = 1;
+  private $per_page = 100;
+  private $method = 'GET';
   /**
    * The name and signature of the console command.
    *
@@ -42,6 +37,7 @@ class DataUpdate extends Command
    */
   public function __construct()
   {
+    $this->token = config('token.qiita');
     parent::__construct();
   }
 
@@ -50,80 +46,118 @@ class DataUpdate extends Command
    *
    * @return int
    */
-  public function handle(Post $post)
+  public function handle()
   {
     // qiita api
-    $client = new Client();
-    $method = 'GET';
-    $day = $this->generate_days(7);
-    $this->data['per_page'] = $this->per_page;
-    $this->data['page'] = $this->page;
-    $this->query_params_set('stocks:>7');
-    $this->query_params_set("created:>=${day}");
-    if($this->query_params) {
-      $this->data['query'] = $this->query_params;
+    // 週間ランキング
+    $this->main(7, 'posts', 5);
+    // メール送信
+    $now = Carbon::now();
+    Mail::send('email.send', ['time' => $now], function($message) {
+      $message->to('aleph0528@gmail.com')
+      ->from('aleph0528@gmail.com', 'qiita-my-ranking')
+      ->subject('Qiitaの週間ランキングのデータを更新しました');
+    });
+    return Command::SUCCESS;
+  }
+
+  // mainの関数 他の関数をまとめた関数
+  /** 
+   * @param int $days 何日以内のデータを取るか 
+   * @param string $db_name 更新するDBのテーブルの名前
+   * @param int $min_stock Qiitaの記事がストックされた数の最低数を決める
+   * @return void Qiitaのデータを取得しDBのデータを更新
+   */
+  public function main($days, $db_name, $min_stock)
+  {
+    $client = new Client;
+    $options = $this->create_request_data($days, $min_stock);
+    $total_contents = $this->get_qiita_posts($client, $this->method, $options);
+    // 並び替え
+    $sort_contents = $this->sort_likes_count($total_contents);
+    // DB用にデータの様式を変更する
+    $db_datas = $this->create_db_datas($sort_contents);
+    $this->update_db($db_name, $db_datas);
+  }
+
+  // データ取得のリクエストデータを作成
+  public function create_request_data($days, $min_stock)
+  {
+    $day = $this->generate_days($days);
+    $data['per_page'] = $this->per_page;
+    $data['page'] = $this->page;
+    $query_params = '';
+    $query_params = $this->query_params_set($query_params, "stocks:>${min_stock}");
+    $query_params = $this->query_params_set($query_params, "created:>=${day}");
+
+    if($query_params != '') {
+      $data['query'] = $query_params;
     }
+
     $options = [
-      'query' => $this->data,
+      'query' => $data,
       'verify' => false,
       'headers' => [
         'Authorization' => 'Bearer '.$this->token,
         'Content-Type' => 'application/json'
       ]
     ];
-    $response = $client->request($method, $this->url, ['verify' => false]);
-    while($this->loop_flag) {
+
+    return $options;
+  }
+
+  // データ取得処理
+  public function get_qiita_posts($client, $method, $options)
+  {
+    $loop_flag = true;
+    $total_contents = [];
+    $count = 0;
+    while($loop_flag) {
       $response = $client->request($method, $this->url, $options);
       $contents = $response->getBody()->getContents();
       $contents = json_decode($contents, true);
       // 追加
-      $this->contents = array_merge($this->contents, $contents);
-      $this->page++;
-      $options['query']['page'] = $this->page;
+      $total_contents = array_merge($total_contents, $contents);
+      $options['query']['page']++;
       // pageの配列が尽きたら
       if(count($contents) != $this->per_page) {
-        $this->loop_flag = false;
-        // 並び替え
-        $result = $this->sortByKey('likes_count', SORT_DESC, $this->contents);
-        $result = array_slice($result, 0, 20);
-        // 古いDBデータ削除
-        DB::table('posts')->delete();
-        foreach ($result as $content) {
-          // DBデータ作成
-          $data = [
-            'post_id' => $content['id'],
-            'post_url' => $content['url'],
-            'title' => $content['title'],
-            'user_image_url' => $content['user']['profile_image_url'],
-            'user_name' => $content['user']['id'],
-            'likes_count' => $content['likes_count'],
-            'created_at' => $content['created_at'],
-            'tags' => json_encode($content['tags'])
-          ];
-          // DB追加
-          $post->create($data);
-          // 不要な部分を削除
-          unset($content['rendered_body']);
-          unset($content['body']);
-          // 何かしらの処理をする時 例:メールで送信する時に渡すコンテンツ
-        }
+        $loop_flag = false;
+        info('合計page数', [$options['query']['page']]);
         break;
       }
+      if($count == 5) {
+        break;
+      }
+      $count++;
     }
+    return $total_contents;
     Log::alert("データを更新しました");
-    $now = Carbon::now();
-    Mail::send('email.send', ['time' => $now], function($message) {
-        $message->to('aleph0528@gmail.com')
-                ->from('aleph0528@gmail.com', 'qiita-my-ranking')
-                ->subject('Qiitaの週間ランキングのデータを更新しました');
-    });
-    return Command::SUCCESS;
+  }
+
+  // データベースの更新
+  public function update_db($db_name, $db_datas)
+  {
+    // 古いDBデータ削除
+    DB::table($db_name)->delete();
+    // DBに保存
+    DB::table($db_name)->insert($db_datas);
+  }
+
+  // LikeCount順に並べ替え
+  public function sort_likes_count($contents)
+  {
+    // 並び替え
+    $result = $this->sortByKey('likes_count', SORT_DESC, $contents);
+    // 20位未満削除
+    $result = array_slice($result, 0, 20);
+    return $result;
   }
 
   // query set
-  public function query_params_set($value)
+  public function query_params_set($query_params, $value)
   {
-    $this->query_params = $this->query_params.' '.$value;
+    $query_params = $query_params.' '.$value;
+    return $query_params;
   }
 
   // 日付求める処理
@@ -140,5 +174,27 @@ class DataUpdate extends Command
     }
     array_multisort($standard_key_array, $sort_order, $array);
     return $array;
+  }
+
+  // データベース用のデータに変更
+  public function create_db_datas($sort_contents)
+  {
+    $db_datas = [];
+    foreach ($sort_contents as $content) {
+      // DB用データ作成
+      $data = [
+        'post_id' => $content['id'],
+        'post_url' => $content['url'],
+        'title' => $content['title'],
+        'user_image_url' => $content['user']['profile_image_url'],
+        'user_name' => $content['user']['id'],
+        'likes_count' => $content['likes_count'],
+        'created_at' => $content['created_at'],
+        'tags' => json_encode($content['tags'])
+      ];
+      // 配列に追加
+      $db_datas[] = $data;
+    }
+    return $db_datas;
   }
 }
