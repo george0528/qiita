9faec6ae07ec75cca3626465308929bb6a588cc1
jota <miki1jota@gmail.com>
2022-02-19 04:30:49 +0900

    update: router-linkを使用

--- front-end/app/src/components/Footer.vue
+++ front-end/app/src/components/Footer.vue
@@ -8,23 +8,29 @@
       background-color="white"
       color="success"
   >
-    <v-btn value="/" to="/" id="btn">
+  <router-link to="/">
+    <v-btn value="/">
       <span>ホーム</span>
 
       <v-icon>mdi-home</v-icon>
     </v-btn>
+  </router-link>
 
-    <v-btn value="/save-contents" to="/save-contents">
+  <router-link to="/save-contents">
+    <v-btn value="/save-contents">
       <span>後で見る</span>
 
       <v-icon>mdi-heart</v-icon>
     </v-btn>
+  </router-link>
 
-    <v-btn value="/history" to="/history">
+  <router-link to="/history">
+    <v-btn value="/history">
       <span>履歴</span>
 
       <v-icon>mdi-history</v-icon>
     </v-btn>
+  </router-link>
   </v-bottom-navigation>
 </div>
 </template>
@@ -41,21 +47,32 @@ export default {
 }
 </script>
 <style scoped>
+* {
+  box-sizing: border-box;
+}
 #footer .space{
   height: 90px;
+}
+.v-item-group.v-bottom-navigation a{
+ height: inherit;
+}
+.router-link-exact-active.router-link-active {
+  color: #4caf50 !important;
 }
 .v-item-group.v-bottom-navigation {
   justify-content: space-around;
 }
 .v-item-group.v-bottom-navigation .v-btn {
   width: 130px;
-  padding-bottom: 20px;
 }
-/* .v-item-group.v-bottom-navigation .v-btn :nth-of-type(1){
+.v-item-group.v-bottom-navigation .v-btn :nth-of-type(1){
   transform: scale(1.1);
-} */
+}
 .v-item-group.v-bottom-navigation .v-btn i {
   display: inline-block;
   margin-bottom: 5px;
+}
+.v-item-group.v-bottom-navigation .v-btn span span {
+  margin-bottom: 20px;
 }
 </style>
