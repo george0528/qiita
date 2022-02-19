ab6ebb724301cba70768411ff8b3326d4caf7f2c
jota <miki1jota@gmail.com>
2022-02-19 18:27:01 +0900

    test: iPhoneのボタン遷移のタブテスト

--- front-end/app/src/App.vue
+++ front-end/app/src/App.vue
@@ -6,6 +6,47 @@
 
     <router-view/>
     <div>
+      <v-card>
+        <v-tabs
+          v-model="tab"
+          background-color="deep-purple accent-4"
+          centered
+          dark
+          icons-and-text
+        >
+          <v-tabs-slider></v-tabs-slider>
+
+          <v-tab to="/">
+            home
+            <v-icon>mdi-phone</v-icon>
+          </v-tab>
+
+          <v-tab to="/save-contents">
+            後で見る
+            <v-icon>mdi-heart</v-icon>
+          </v-tab>
+
+          <v-tab to="/history">
+            history
+            <v-icon>mdi-account-box</v-icon>
+          </v-tab>
+        </v-tabs>
+
+        <v-tabs-items v-model="tab">
+          <v-tab-item
+            v-for="i in 3"
+            :key="i"
+            :value="'tab-' + i"
+          >
+            <v-card flat>
+              <v-card-text>{{ text }}</v-card-text>
+            </v-card>
+          </v-tab-item>
+        </v-tabs-items>
+      </v-card>
+
+    </div>
+    <div>
       <v-bottom-nav
         absolute
       >
