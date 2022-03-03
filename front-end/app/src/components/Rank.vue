9600f4653debab2bc5a361a29be5842e4e50eff4
jota <aleph0528@gmail.com>
2022-03-03 23:44:43 +0900

    style: ランキングの数字と王冠の位置がずれていたため

--- front-end/app/src/components/Rank.vue
+++ front-end/app/src/components/Rank.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>
+  <div id="Rank">
     <crown :num="index + 1" />
     <span class="rank_index">{{ index + 1 }}</span>
   </div>
 </template>
+<style scoped>
+  div {
+    display: flex;
+    align-items: center;
+  }
+</style>
 
 <script>
 import Crown from './Crown.vue'
