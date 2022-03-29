fdc1f7d021eafae990212723d2a32635189df9df
jota <aleph0528@gmail.com>
2022-03-29 23:38:44 +0900

    update: vue.config.jsにbuild時のメモリの上限を増やすかも？の設定を追加

--- front-end/app/vue.config.js
+++ front-end/app/vue.config.js
@@ -1,3 +1,5 @@
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
 module.exports = {
     publicPath: './',
     pwa: {
@@ -12,4 +14,25 @@ module.exports = {
             openAnalyzer: false
         },
     },
+    // build時のメモリの上限の設定 参考(https://jpcodeqa.com/q/9657df44a2eef94b0faa692fcdc0ef7b)
+    configureWebpack: config => {
+
+        // get a reference to the existing ForkTsCheckerWebpackPlugin
+        const existingForkTsChecker = config.plugins.filter(
+          p => p instanceof ForkTsCheckerWebpackPlugin,
+        )[0];
+    
+        // remove the existing ForkTsCheckerWebpackPlugin
+        // so that we can replace it with our modified version
+        config.plugins = config.plugins.filter(
+          p => !(p instanceof ForkTsCheckerWebpackPlugin),
+        );
+    
+        // copy the options from the original ForkTsCheckerWebpackPlugin
+        // instance and add the memoryLimit property
+        const forkTsCheckerOptions = existingForkTsChecker.options;
+        forkTsCheckerOptions.memoryLimit =  6000;
+    
+        config.plugins.push(new ForkTsCheckerWebpackPlugin(forkTsCheckerOptions));
+      },
 }
