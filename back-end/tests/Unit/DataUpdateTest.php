05d8eb2c9c56cc8f7fccd175bcfb9949c2eebb59
jota <aleph0528@gmail.com>
2022-04-05 22:01:28 +0900

    test: DataUpdateクラスのリクエストデータの作成メソッドのテストを追加

---
+++ back-end/tests/Unit/DataUpdateTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Console\Commands\DataUpdate;
+use Carbon\Carbon;
+use Tests\TestCase;
+
+class DataUpdateTest extends TestCase
+{
+    // テストで使う DataUpdateクラスのprivateクラス変数
+    private $url = 'https://qiita.com/api/v2/items/';
+    private $page = 1;
+    private $per_page = 100;
+    private $method = 'GET';
+
+    // セットアップ
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->class = new DataUpdate();
+    }
+
+    public function test_test()
+    {
+        $this->assertEquals(1, 1);
+    }
+    
+    public function test_generate_day()
+    {
+        $day = 3;
+        $date = new Carbon("-${day} day");
+        $expectation = $date->format("Y-m-d");
+
+        $result = $this->class->generate_days($day);
+        
+        $this->assertEquals($result, $expectation);
+    }
+
+    public function test_query_params_set()
+    {
+        $result = $this->class->query_params_set('', 'test');
+        
+        $this->assertEquals($result, ' test');
+    }
+
+    public function test_create_request_data()
+    {
+        $day = 3;
+        $minStock = 10;
+        $minCreatedDate = $this->class->generate_days($day);
+
+        $data['per_page'] = $this->per_page;
+        $data['page'] = $this->page;
+        $data['query'] = " stocks:>${minStock} created:>=${minCreatedDate}";
+
+        $options = [
+            'query' => $data,
+            'verify' => false,
+            'headers' => [
+              'Authorization' => 'Bearer '.$this->class->token,
+              'Content-Type' => 'application/json'
+            ]
+        ];
+
+        $result = $this->class->create_request_data($day, $minStock);
+        $this->assertEquals($options, $result);
+    }
+}
