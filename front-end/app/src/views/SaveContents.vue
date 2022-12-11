526489943d8fa4976bba91223918b9aac35505bb
jota <aleph0528@gmail.com>
2022-12-12 03:48:40 +0900

    Revert "update: consoleに表示されるエラー修正"
    This reverts commit f942378530102fc6a71c94fd6c6ee3f4ee2fee38.

--- front-end/app/src/views/SaveContents.vue
+++ front-end/app/src/views/SaveContents.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="SaveContent">
     <div class="top_space"></div>
-    <Contents
+    <Contents 
       :contents="this.contents"
       :is_include_id="this.is_include_id"
       :rank="false"
@@ -64,10 +64,8 @@ export default {
     this.url = new URL('https://back-end.qiita-my-ranking.online/api/ranking');
     this.get_save_contents();
     this.contents = this.save_contents;
-    if(this.contents.value != undefined) {
-      this.contents.reverse();
-    }
-    window.scrollTo(0, 0);
+    this.contents.reverse();
+    window.scrollTo(0, 0); 
   }
 }
 </script>
